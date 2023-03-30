7dd97198c17046fa7fd79662e313758dadb7b169
Wiktoria Assman <wa117777@stud.ur.edu.pl>
2023-03-30 12:33:41 +0200

    lab3 -done

---
+++ Functions/ex1.py
@@ -0,0 +1,16 @@
+def multiply(a, b):
+    return a * b
+    print('to się nigdy nie wykona')
+
+print(multiply(10, 2))
+
+
+def greeting(name):
+    print('Hello ' + name)
+
+greeting('Marcin')
+
+
+
+result = greeting('Marcin')
+print(result) # wynikiem działania funkcji greeting jest None
