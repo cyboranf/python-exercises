1c410ff441ca70a1db0b9445c79e41a6405bb236
Karol66 <karolbal66@gmail.com>
2023-04-20 11:59:49 +0200

    operator examples -added

---
+++ ThirdLabs/Operators.py
@@ -0,0 +1,16 @@
+a = 5
+b = 10
+
+print((a - b) / 2 >= 0)
+
+print(True and (2 < 3))
+
+print((a < a and a < b) or True)
+
+print((a == a or a < b) and (a == a or a == b))
+
+print(not ((a == a or a < b) and (a == a or a == b)))
+
+
+print(a & b)
+print(a | b)
